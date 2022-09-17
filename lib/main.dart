fb99dcbc6da4999653393cde59e9d3e88dea33d3
xtyao <xtyao@bu.edu>
2022-09-16 23:37:40 -0400

    Data requests from twitter is completed
    Can now request data from Twitter for analysis, the botometer backend is connected. Still has some problems with data types.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:twitter_api_v2/twitter_api_v2.dart' as v2;
+import 'package:twitter_api_v2/twitter_api_v2.dart';
+
+import 'package:twitter_oauth2_pkce/twitter_oauth2_pkce.dart';
 
 import 'dart:developer';
 import 'dart:async';
@@ -63,6 +67,9 @@ class _MyWidgetState extends State<MyWidget> {
           onPressed: () async {
             ReturnedText = "place_holder";
             ReturnedText = await something(textController.text);
+            List results = await do_stuff("TwitterDev");
+            await botometer(results);
+
             setState(() {
               displayText = ReturnedText;
             });
@@ -92,4 +99,97 @@ Future<String> something(String username) async {
 
   return response.body;
   // return "some_future_text";
+}
+
+Future<List> do_stuff(String usrName) async {
+  //! You need to get keys and tokens at https://developer.twitter.com
+  final twitter = v2.TwitterApi(
+    //! Authentication with OAuth2.0 is the default.
+    //!
+    //! Note that to use endpoints that require certain user permissions,
+    //! such as Tweets and Likes, you need a token issued by OAuth2.0 PKCE.
+    //!
+    //! The easiest way to achieve authentication with OAuth 2.0 PKCE is
+    //! to use [twitter_oauth2_pkce](https://pub.dev/packages/twitter_oauth2_pkce)!
+    bearerToken:
+        'AAAAAAAAAAAAAAAAAAAAAPSZgwEAAAAAbRTtPV6SA0splpK4DMbG%2BOz2aso%3DKsHqLjCKHx6C4RD8EsInMwYhEIXxUkbMlcPx0RkFUONZq92g0s',
+
+    //! Or perhaps you would prefer to use the good old OAuth1.0a method
+    //! over the OAuth2.0 PKCE method. Then you can use the following code
+    //! to set the OAuth1.0a tokens.
+    //!
+    //! However, note that some endpoints cannot be used for OAuth 1.0a method
+    //! authentication.
+    oauthTokens: v2.OAuthTokens(
+      consumerKey: 'c5uB923Kxhmm4CbTN2H9xbd5w',
+      consumerSecret: 'ubA7OYSwgvsxjHsv9DRWHgY7Z466BWLS1fbEcuKSAd4JtAVKo2',
+      accessToken: '1568054324311343106-q5JIqaAInwtOAvfWDBrEb7xKgOE3s9',
+      accessTokenSecret: 'lUAZ0ua2ck8XiINxmkGjTbGtAFhWNhnW13lRjEU8ufPeJ',
+    ),
+
+    //! Automatic retry is available when a TimeoutException occurs when
+    //! communicating with the API.
+    retryConfig: v2.RetryConfig.ofRegularIntervals(
+      maxAttempts: 5,
+      intervalInSeconds: 3,
+    ),
+
+    //! The default timeout is 10 seconds.
+    timeout: Duration(seconds: 20),
+  );
+
+  try {
+    // Get the authenticated user's profile.
+    final targetUser = await twitter.usersService.lookupByName(
+      username: usrName,
+    );
+
+    String userId = targetUser.data.id;
+    // log(userId);
+    // Get the tweets associated with the search query.
+    final timeline = await twitter.tweetsService.lookupTweets(
+      userId: userId,
+      maxResults: 100,
+    );
+
+    final mentions = await twitter.tweetsService.lookupMentions(
+      userId: userId,
+      maxResults: 100,
+    );
+
+    return [targetUser, timeline, mentions];
+  } on TimeoutException catch (e) {
+    print(e);
+  } on v2.UnauthorizedException catch (e) {
+    print(e);
+  } on v2.RateLimitExceededException catch (e) {
+    print(e);
+  } on v2.TwitterException catch (e) {
+    print(e.response.headers);
+    print(e.body);
+    print(e);
+  }
+  return ["something is wrong"];
+}
+
+Future<String> botometer(List results) async {
+  var uri = Uri.parse('https://botometer-pro.p.rapidapi.com/4/check_account');
+
+  var response = await http.post(
+    uri,
+    body: jsonEncode(<String, List>{
+      'data': results,
+    }),
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '84024ab43bmsha3a872b3d68e5bdp1584e6jsn8d08ba92e9bf',
+      'X-RapidAPI-Host': 'botometer-pro.p.rapidapi.com'
+    },
+  );
+
+  log('Response status: ${response.statusCode}');
+  log('Response body: ${response.body}');
+
+  log(response.body);
+  return response.body;
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import flutter_web_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
 }
