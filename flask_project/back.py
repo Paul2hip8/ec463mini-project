286617ccb8fd70641481a0257ee1c4b70740b64c
Paul2hip8 <par1227@bu.edu>
2022-09-22 12:39:06 -0400

    Update back.py
    editing out some unneeded statements

--- flask_project/back.py
+++ flask_project/back.py
@@ -18,21 +18,9 @@ bom = botometer.Botometer(wait_on_ratelimit=True,
                           **twitter_app_auth)
 
 def sample_analyze_entity_sentiment(text_content):
-    """
-    Analyzing Entity Sentiment in a String
-
-    Args:
-      text_content The text content to analyze
-    """
-
     client = language_v1.LanguageServiceClient()
-
     # text_content = 'Grapes are good. Bananas are bad.'
-
-    # Available types: PLAIN_TEXT, HTML
     type_ = language_v1.types.Document.Type.PLAIN_TEXT
-
-    # Available values: NONE, UTF8, UTF16, UTF32
     encoding_type = language_v1.EncodingType.UTF8
 
     response = client.analyze_entity_sentiment(request = {'document': document, 'encoding_type': encoding_type})
@@ -47,25 +35,7 @@ def sample_analyze_entity_sentiment(text_content):
         sentiment = entity.sentiment
         print(u"Entity sentiment score: {}".format(sentiment.score))
         print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
-        # Loop over the metadata associated with entity. For many known entities,
-        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
-        # Some entity types may have additional metadata, e.g. ADDRESS entities
-        # may have metadata for the address street_name, postal_code, et al.
-        for metadata_name, metadata_value in entity.metadata.items():
-            print(u"{} = {}".format(metadata_name, metadata_value))
 
-        # Loop over the mentions of this entity in the input document.
-        # The API currently supports proper noun mentions.
-        for mention in entity.mentions:
-            print(u"Mention text: {}".format(mention.text.content))
-            # Get the mention type, e.g. PROPER for proper noun
-            print(
-                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
-            )
-
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
     print(u"Language of the text: {}".format(response.language))
 
 def _find_next_id():
