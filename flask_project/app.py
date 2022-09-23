bc81349bc1227ebf7994668f7701566f21f516bb
xtyao <xtyao@bu.edu>
2022-09-22 22:16:30 -0400

    finished almost everything

--- flask_project/app.py
+++ flask_project/app.py
@@ -5,22 +5,25 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 from wtforms import SubmitField
 
+import json
+
 import os
     
+import requests
 import botometer
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-rapidapi_key = "84024ab43bmsha3a872b3d68e5bdp1584e6jsn8d08ba92e9bf"
-twitter_app_auth = {
-    'consumer_key': 'c5uB923Kxhmm4CbTN2H9xbd5w',
-    'consumer_secret': 'ubA7OYSwgvsxjHsv9DRWHgY7Z466BWLS1fbEcuKSAd4JtAVKo2',
-    'access_token': '1568054324311343106-q5JIqaAInwtOAvfWDBrEb7xKgOE3s9',
-    'access_token_secret': 'lUAZ0ua2ck8XiINxmkGjTbGtAFhWNhnW13lRjEU8ufPeJ',
-  }
-bom = botometer.Botometer(wait_on_ratelimit=True,
-                          rapidapi_key=rapidapi_key,
-                          **twitter_app_auth)
+# rapidapi_key = "84024ab43bmsha3a872b3d68e5bdp1584e6jsn8d08ba92e9bf"
+# twitter_app_auth = {
+#     'consumer_key': 'c5uB923Kxhmm4CbTN2H9xbd5w',
+#     'consumer_secret': 'ubA7OYSwgvsxjHsv9DRWHgY7Z466BWLS1fbEcuKSAd4JtAVKo2',
+#     'access_token': '1568054324311343106-q5JIqaAInwtOAvfWDBrEb7xKgOE3s9',
+#     'access_token_secret': 'lUAZ0ua2ck8XiINxmkGjTbGtAFhWNhnW13lRjEU8ufPeJ',
+#   }
+# bom = botometer.Botometer(wait_on_ratelimit=True,
+#                           rapidapi_key=rapidapi_key,
+#                           **twitter_app_auth)
 
 app = Flask(__name__) # create our flask app
 
@@ -42,13 +45,30 @@ def my_form_post():
     error_message =""
     # Check a single account by screen name
     try:
-        result = bom.check_account(id);
+        result = requests.get('http://127.0.0.1:20050/users/'+id)
+        
+        result = result.json()
         dataEn= result.get('display_scores').get('english')
+        # print(dataEn)
         dataUn = result.get('display_scores').get('universal')
-        return render_template('index.html',id = id, dataEn = dataEn, dataUn = dataUn)
+        
     except:
         error_message = "can't score the user"
         return render_template('first_time.html',error_message = error_message)
+    
+    try:
+        res = requests.get('http://127.0.0.1:20050/sent/'+id)
+        
+        res = res.json()
+        tweet_text = res.get('text')
+        print(tweet_text)
+        tweet_score = res.get('score')
+        message2 = "the user's most recent tweet is: \n" + tweet_text 
+        message3 = "and the sentiment score is: " + str(tweet_score)+ " from -1.0 to +1.0, the higher the score, the more positive the tweet is"
+    except:
+        message2 = "user does not have tweets"
+    
+    return render_template('index.html',id = id, dataEn = dataEn, dataUn = dataUn, message2 = message2, message3 = message3)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask_project/back.py
+++ flask_project/back.py
@@ -1,29 +1,82 @@
 # app.py
 from flask import Flask, request, jsonify
 import botometer
+from google.cloud import language_v1
+import six
+import tweepy
+import json
 
 app = Flask(__name__)
 
+consumer_key = "c5uB923Kxhmm4CbTN2H9xbd5w"
+consumer_secret = "ubA7OYSwgvsxjHsv9DRWHgY7Z466BWLS1fbEcuKSAd4JtAVKo2"
+access_token = "1568054324311343106-q5JIqaAInwtOAvfWDBrEb7xKgOE3s9"
+access_token_secret = "lUAZ0ua2ck8XiINxmkGjTbGtAFhWNhnW13lRjEU8ufPeJ"
+
 rapidapi_key = "84024ab43bmsha3a872b3d68e5bdp1584e6jsn8d08ba92e9bf"
 twitter_app_auth = {
-    'consumer_key': 'c5uB923Kxhmm4CbTN2H9xbd5w',
-    'consumer_secret': 'ubA7OYSwgvsxjHsv9DRWHgY7Z466BWLS1fbEcuKSAd4JtAVKo2',
-    'access_token': '1568054324311343106-q5JIqaAInwtOAvfWDBrEb7xKgOE3s9',
-    'access_token_secret': 'lUAZ0ua2ck8XiINxmkGjTbGtAFhWNhnW13lRjEU8ufPeJ',
+    'consumer_key': consumer_key,
+    'consumer_secret': consumer_secret,
+    'access_token': access_token,
+    'access_token_secret': access_token_secret,
   }
 bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
 
 
+@app.route('/')
+def home_page():
+    return '<h1>Welcome to my site!</h1>'
 
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
-
-@app.get("/countries")
-def get_countries(id):
-    if len(id) == 0:
-        return {"error": "empty request"}, 415
+@app.route('/users/<user_id>', methods = ['GET'])
+def user(user_id):
+    if request.method == 'GET':
+        result = bom.check_account(user_id);
+        return result, 200
     else:
-        result = bom.check_account(id)
-        return jsonify(result)
+        return {"error": "Request must be JSON"}, 415
+
+@app.route('/sent/<screen_name>', methods = ['GET'])
+def sample_analyze_sentiment(screen_name):
+
+    # authorization of consumer key and consumer secret
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    
+    # set access to user's access key and access secret 
+    auth.set_access_token(access_token, access_token_secret)
+    
+    # calling the api 
+    api = tweepy.API(auth)
+    
+    # screen name of the account to be fetched
+    
+    # fetching the statuses
+    statuses = api.user_timeline(screen_name = screen_name, count = 5, include_rts = False)
+    
+    client = language_v1.LanguageServiceClient()
+
+    if len(statuses) <= 0:
+        return "no tweets found", 404
+    else:
+        return_json = {
+            
+        }
+        content = statuses[0].text
+        if isinstance(content, six.binary_type):
+            content = content.decode("utf-8")
+
+        type_ = language_v1.Document.Type.PLAIN_TEXT
+        document = {"type_": type_, "content": content}
+
+        response = client.analyze_sentiment(request={"document": document})
+        sentiment = response.document_sentiment
+        
+        return_json["score"] = sentiment.score
+        return_json["text"] =content
+    
+    return json.dumps(return_json), 200
+
+    
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=20050, debug=True)
